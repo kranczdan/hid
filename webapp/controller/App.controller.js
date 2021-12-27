b6dc6d3e293e7cda665438da6dd336b33fffb711
Daniel Krancz <90087919+kranczdan@users.noreply.github.com>
2021-12-27 22:53:06 +0100

    Initial commit

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("at.fhburgenland.HID.controller.App", {
+		onInit: function () {
+
+		}
+	});
+});
